c17dd528c0eceaf391f990280c94fd8cba7958f7
jannisalsbach <jannisalsbach@gmail.com>
2024-03-21 13:01:01 +0100

    Refactor login and grade sending functionality

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -2,49 +2,50 @@ import '../app/globals.css';
 
 import React from 'react';
 
-const handleLogin = () => {
-  window.location.href = '/login';
-};
 
-const [lineItemUrl, setLineItemUrl] = useState('');
-
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
-        const lineItem = params.get('lineItemUrl');
-        if (lineItem) {
-            setLineItemUrl(decodeURIComponent(lineItem));
-        }
-    }, []);
-
-    const sendGrade = async () => {
-      const ngrokUrl = 'https://4559-89-206-81-172.ngrok-free.app ';
-      const scorePayload = {
-        score: 0.7,
-        lineItemId: lineItemUrl,
-      };
-    
-      try {
-        const res = await fetch(`${ngrokUrl}/api/send-grade`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(scorePayload),
-        });
-    
-        if (res.ok) {
-          const data = await res.json();
-          console.log("Bewertung erfolgreich gesendet: ", data);
-        } else {
-          console.error('Fehler beim Senden der Bewertung:', res.statusText);
-        }
-      } catch (error) {
-        console.error('Fehler beim Senden der Bewertung:', error);
-      }
-    };
     
 
 const HomePage = () => {
+  const handleLogin = () => {
+    window.location.href = '/login';
+  };
+  
+  const [lineItemUrl, setLineItemUrl] = React.useState('');
+  
+  React.useEffect(() => {
+      const params = new URLSearchParams(window.location.search);
+      const lineItem = params.get('lineItemUrl');
+      if (lineItem) {
+          setLineItemUrl(decodeURIComponent(lineItem));
+      }
+  }, []);
+  
+  const sendGrade = async () => {
+    const ngrokUrl = 'https://4559-89-206-81-172.ngrok-free.app ';
+    const scorePayload = {
+      score: 0.7,
+      lineItemId: lineItemUrl,
+    };
+  
+    try {
+      const res = await fetch(`${ngrokUrl}/grade`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(scorePayload),
+      });
+  
+      if (res.ok) {
+        const data = await res.json();
+        console.log("Bewertung erfolgreich gesendet: ", data);
+      } else {
+        console.error('Fehler beim Senden der Bewertung:', res.statusText);
+      }
+    } catch (error) {
+      console.error('Fehler beim Senden der Bewertung:', error);
+    }
+  };
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-semibold mb-4">LTI Demo App</h1>
